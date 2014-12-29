94cbbb582f34657b845a63f6f039d1a6c81a9f8b
Jay Satiro <raysatiro@yahoo.com>
2014-12-29 03:29:28 -0500

    Many bugfixes, enhance logging, loop worker option
    - Add support for old MinGW compilers.
    The original MinGW project has includes that are missing some required
    items like TOKEN_ADJUST_SESSIONID.
    
    - Add advapi32 lib pragma for VS compilers.
    
    - Fix Unicode build errors.
    Although it appears the author's intent was to offer a choice to build
    ANSI or Unicode, prior to this change the Unicode build did not compile.
    
    - Change ServiceMain and ControlHandlerEx to WINAPI (__stdcall).
    Prior to this change both functions were incorrectly of type __cdecl.
    
    - Fix other compiler warnings.
    Use type prefix I32 (eg %I32u) to print DWORDs.
    
    - Implement proper cleanup for WorkerThread and ServiceMain.
    Take care that all variables are initialized and de-initialized
    properly. Ensure that any invalid handle is set to INVALID_HANDLE_VALUE
    and test against that value before closing a handle.
    
    - Serialize logging.
    Prior to this change logging wasn't thread safe.
    
    - New function macro logf_gle.
    Like logf to log but also call GetLastError() and include the result in
    the log entry.
    
    - Add build option via define LOOP_WORKER to loop the worker thread.
    Prior to this change the only behavior was that the service stopped
    after executing the target process in the session that was next
    connected to. Now if built with LOOP_WORKER the worker thread loops and
    for each console connection made the target process is executed in the
    respective session.
    
    - Update README.md.
    Include some basic info, known issues and Winlogon references.

--- QTestService.c
+++ QTestService.c
@@ -5,119 +5,252 @@ http://omeg.pl/blog/2014/01/running-processes-on-the-winlogon-desktop/
 https://github.com/jay/QTestService
 
 This service works to run a command on the winlogon desktop during switch user. It may take several
-seconds before the command runs, not sure why. The service stops after it runs the command.
+seconds before the command runs, not sure why. The service stops after it runs the command unless
+you define LOOP_WORKER, in which case it continues to run and monitor for session connections.
 
 gcc -o QTestService QTestService.c
+gcc -DLOOP_WORKER -o QTestService QTestService.c
+
+If you are making a Unicode build you must define both UNICODE and _UNICODE.
 
 sc create QTestService binPath= X:\code\winlogon\QTestService\QTestService.exe start= demand
 sc start QTestService
 */
 
-#include <windows.h>
+#define   _CRT_SECURE_NO_WARNINGS
 
-//#include "log.h"
+#include <windows.h>
+#include <tchar.h>
 #include <stdio.h>
 
-FILE *g_fp;
+#ifdef _MSC_VER
+#pragma comment(lib, "advapi32.lib")
+#endif
 
-#define log_init(unknown1, unknown2) do { \
-g_fp = fopen("X:\\code\\winlogon\\QTestService\\QTestService.log", "a"); \
-if(g_fp) { fprintf(g_fp, "\n\n"); fflush(g_fp); } \
-} while(0)
 
-#define logf(...) do { \
-    if(g_fp) { \
+// begin: stuff for older mingw
+#ifndef WTS_CONSOLE_CONNECT
+#define WTS_CONSOLE_CONNECT                0x1
+#define WTS_CONSOLE_DISCONNECT             0x2
+#define WTS_REMOTE_CONNECT                 0x3
+#define WTS_REMOTE_DISCONNECT              0x4
+#define WTS_SESSION_LOGON                  0x5
+#define WTS_SESSION_LOGOFF                 0x6
+#define WTS_SESSION_LOCK                   0x7
+#define WTS_SESSION_UNLOCK                 0x8
+#define WTS_SESSION_REMOTE_CONTROL         0x9
+typedef struct tagWTSSESSION_NOTIFICATION
+{
+    DWORD cbSize;
+    DWORD dwSessionId;
+} WTSSESSION_NOTIFICATION, *PWTSSESSION_NOTIFICATION;
+#endif
+
+#ifndef TOKEN_ADJUST_SESSIONID
+#define TOKEN_ADJUST_SESSIONID   (0x0100)
+#undef TOKEN_ALL_ACCESS
+#define TOKEN_ALL_ACCESS (STANDARD_RIGHTS_REQUIRED |\
+                          TOKEN_ASSIGN_PRIMARY     |\
+                          TOKEN_DUPLICATE          |\
+                          TOKEN_IMPERSONATE        |\
+                          TOKEN_QUERY              |\
+                          TOKEN_QUERY_SOURCE       |\
+                          TOKEN_ADJUST_PRIVILEGES  |\
+                          TOKEN_ADJUST_GROUPS      |\
+                          TOKEN_ADJUST_DEFAULT     |\
+                          TOKEN_ADJUST_SESSIONID)
+#endif
+
+#ifndef RTL_NUMBER_OF
+#define RTL_NUMBER_OF(A)   (sizeof(A)/sizeof((A)[0]))
+#endif
+// end: stuff for older mingw
+
+
+FILE *g_log_fp;
+CRITICAL_SECTION g_log_lock;
+
+#ifdef _UNICODE
+#define PRINT_VA(...)   fwprintf(g_log_fp, L##__VA_ARGS__)
+#else
+#define PRINT_VA(...)   fprintf(g_log_fp, __VA_ARGS__)
+#endif
+
+// This is meant to be called by the other function macros.
+// GetLastError must be the first call to accurately get the last error!
+#define plumb_logf(show_gle, ...) do { \
+    if(g_log_fp) { \
+        DWORD gle; \
         SYSTEMTIME t; \
+        gle = GetLastError(); \
+        EnterCriticalSection(&g_log_lock); \
         GetLocalTime(&t); \
-        fprintf(g_fp, "%04u-%02u-%02u %02u:%02u:%02u.%03u - ", \
+        _ftprintf(g_log_fp, TEXT("%04u-%02u-%02u %02u:%02u:%02u.%03u - "), \
             t.wYear, t.wMonth, t.wDay, t.wHour, t.wMinute, t.wSecond, t.wMilliseconds); \
-        fprintf(g_fp, __VA_ARGS__); \
-        fprintf(g_fp, "\n"); \
-        fflush(g_fp); \
+        PRINT_VA(__VA_ARGS__); \
+        if(show_gle) { \
+            _ftprintf(g_log_fp, TEXT(", GetLastError: %I32u"), gle); \
+        } \
+        _ftprintf(g_log_fp, TEXT("\n")); \
+        fflush(g_log_fp); \
+        LeaveCriticalSection(&g_log_lock); \
     } \
 } while(0)
+
+// log and don't append last error
+#define logf(...)   plumb_logf(FALSE, __VA_ARGS__)
+
+// log and append last error from GetLastError
+#define logf_gle(...)   plumb_logf(TRUE, __VA_ARGS__)
+
 
 #define SERVICE_NAME TEXT("QTestService")
 
 SERVICE_STATUS g_Status;
-SERVICE_STATUS_HANDLE g_StatusHandle;
-HANDLE g_ConsoleEvent;
+SERVICE_STATUS_HANDLE g_StatusHandle; // this isn't a regular handle it's a struct
+HANDLE g_ConsoleEvent = INVALID_HANDLE_VALUE;
 DWORD g_TargetSessionId;
 
-void ServiceMain(unsigned long argc, TCHAR *argv[]);
-DWORD ControlHandlerEx(DWORD controlCode, DWORD eventType, void *eventData, void *context);
+void WINAPI ServiceMain(DWORD argc, TCHAR *argv[]);
+DWORD WINAPI ControlHandlerEx(DWORD controlCode, DWORD eventType, void *eventData, void *context);
 
-WCHAR *g_SessionEventName[] = {
-    L"<invalid>",
-    L"WTS_CONSOLE_CONNECT",
-    L"WTS_CONSOLE_DISCONNECT",
-    L"WTS_REMOTE_CONNECT",
-    L"WTS_REMOTE_DISCONNECT",
-    L"WTS_SESSION_LOGON",
-    L"WTS_SESSION_LOGOFF",
-    L"WTS_SESSION_LOCK",
-    L"WTS_SESSION_UNLOCK",
-    L"WTS_SESSION_REMOTE_CONTROL",
-    L"WTS_SESSION_CREATE",
-    L"WTS_SESSION_TERMINATE"
+TCHAR *g_SessionEventName[] = {
+    TEXT("<invalid>"),
+    TEXT("WTS_CONSOLE_CONNECT"),
+    TEXT("WTS_CONSOLE_DISCONNECT"),
+    TEXT("WTS_REMOTE_CONNECT"),
+    TEXT("WTS_REMOTE_DISCONNECT"),
+    TEXT("WTS_SESSION_LOGON"),
+    TEXT("WTS_SESSION_LOGOFF"),
+    TEXT("WTS_SESSION_LOCK"),
+    TEXT("WTS_SESSION_UNLOCK"),
+    TEXT("WTS_SESSION_REMOTE_CONTROL"),
+    TEXT("WTS_SESSION_CREATE"),
+    TEXT("WTS_SESSION_TERMINATE")
 };
 
 // Entry point.
-int main(int argc, TCHAR *argv[])
+int main(int argc, char *argv[])
 {
     SERVICE_TABLE_ENTRY serviceTable[] = {
         {SERVICE_NAME, ServiceMain},
         {NULL, NULL}
     };
 
-    log_init(NULL, TEXT("qservice"));
+    g_log_fp = _tfopen(TEXT("X:\\code\\winlogon\\QTestService\\QTestService.log"), TEXT("a"));
+    if(g_log_fp)
+    {
+        _ftprintf(g_log_fp, TEXT("\n\n"));
+        fflush(g_log_fp);
+        InitializeCriticalSection(&g_log_lock);
+    }
+
     logf("main: start");
-    StartServiceCtrlDispatcher(serviceTable);
+    if(!StartServiceCtrlDispatcher(serviceTable))
+    {
+        logf_gle("StartServiceCtrlDispatcher failed");
+    }
     logf("main: end");
     return 0;
 }
 
 DWORD WINAPI WorkerThread(void *param)
 {
-    TCHAR *cmdline;
-    PROCESS_INFORMATION pi;
-    STARTUPINFO si;
-    HANDLE newToken;
-    DWORD sessionId;
-    DWORD size;
-    HANDLE currentToken;
+    DWORD exitcode = ERROR_SUCCESS;
+    TCHAR *cmdline = NULL;
+    size_t cmdline_len = 0;
+    PROCESS_INFORMATION pi = { INVALID_HANDLE_VALUE, INVALID_HANDLE_VALUE, 0, 0 };
+    STARTUPINFO si = { 0, };
+    HANDLE newToken = INVALID_HANDLE_VALUE;
+    DWORD sessionId = 0;
+    DWORD size = 0;
+    HANDLE currentToken = INVALID_HANDLE_VALUE;
     HANDLE currentProcess = GetCurrentProcess();
 
-    cmdline = (TCHAR*) param;
+    if(!param)
+    {
+        logf("Error: No command line");
+        exitcode = ERROR_INVALID_PARAMETER;
+        goto cleanup;
+    }
+
+    // Older MinGW doesn't have strnlen
+    {
+        void *p;
+#ifdef _UNICODE
+        p = wmemchr(
+#else
+        p = memchr(
+#endif
+            (TCHAR *)param, 0, 32768);
+
+        if(!p)
+        {
+            logf("Error: Command line is too long");
+            exitcode = ERROR_INVALID_PARAMETER;
+            goto cleanup;
+        }
+
+        cmdline_len = ((char *)p - (char *)param) / sizeof(TCHAR);
+    }
+
+    cmdline = malloc((cmdline_len + 1) * sizeof(TCHAR) );
+    if(!cmdline)
+    {
+        logf("Error: malloc failed, not enough memory for command line");
+        exitcode = ERROR_NOT_ENOUGH_MEMORY;
+        goto cleanup;
+    }
+
+#ifdef LOOP_WORKER
+loop:
+#endif
+
+    memcpy(cmdline, param, (cmdline_len + 1) * sizeof(TCHAR));
 
     // Wait until the interactive session changes (to winlogon console).
+    logf("Waiting for event g_ConsoleEvent");
     WaitForSingleObject(g_ConsoleEvent, INFINITE);
+    logf("Received event g_ConsoleEvent");
 
     // Get access token from ourselves.
-    OpenProcessToken(currentProcess, TOKEN_ALL_ACCESS, &currentToken);
+    if(!OpenProcessToken(currentProcess, TOKEN_ALL_ACCESS, &currentToken))
+    {
+        exitcode = GetLastError();
+        logf_gle("Error: OpenProcessToken failed");
+        goto cleanup;
+    }
+
     // Session ID is stored in the access token. For services it's normally 0.
-    GetTokenInformation(currentToken, TokenSessionId, &sessionId, sizeof(sessionId), &size);
-    logf("current session: %d", sessionId);
+    if(!GetTokenInformation(currentToken, TokenSessionId, &sessionId, sizeof(sessionId), &size))
+    {
+        exitcode = GetLastError();
+        logf_gle("Error: GetTokenInformation failed");
+        goto cleanup;
+    }
+    logf("current session: %I32u", sessionId);
 
     // We need to create a primary token for CreateProcessAsUser.
     if (!DuplicateTokenEx(currentToken, TOKEN_ALL_ACCESS, NULL,
         SecurityImpersonation, TokenPrimary, &newToken))
     {
-        perror("DuplicateToken");
-        return GetLastError();
+        exitcode = GetLastError();
+        logf_gle("Error: DuplicateTokenEx failed");
+        goto cleanup;
     }
-    CloseHandle(currentToken);
 
     // g_TargetSessionId is set by SessionChange() handler after a WTS_CONSOLE_CONNECT event.
     // Its value is the new console session ID. In our case it's the "logon screen".
     sessionId = g_TargetSessionId;
-    logf("Running process '%s' in session %d", cmdline, sessionId);
+    logf("Running '%s' in session %I32u", cmdline, sessionId);
+
     // Change the session ID in the new access token to the target session ID.
     // This requires SeTcbPrivilege, but we're running as SYSTEM and have it.
     if (!SetTokenInformation(newToken, TokenSessionId, &sessionId, sizeof(sessionId)))
     {
-        perror("SetTokenInformation(TokenSessionId)");
-        return GetLastError();
+        exitcode = GetLastError();
+        logf_gle("Error: SetTokenInformation failed");
+        goto cleanup;
     }
 
     // Create process with the new token.
@@ -127,22 +260,52 @@ DWORD WINAPI WorkerThread(void *param)
     si.lpDesktop = TEXT("WinSta0\\Winlogon");
     if (!CreateProcessAsUser(newToken, 0, cmdline, 0, 0, 0, 0, 0, 0, &si, &pi))
     {
-        perror("CreateProcessAsUser");
-        return GetLastError();
+        exitcode = GetLastError();
+        logf_gle("Error: CreateProcessAsUser failed");
+        goto cleanup;
     }
-    CloseHandle(newToken);
-    CloseHandle(pi.hThread);
-    CloseHandle(pi.hProcess);
 
-    return ERROR_SUCCESS;
+cleanup:
+    if(currentToken != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(currentToken);
+        currentToken = INVALID_HANDLE_VALUE;
+    }
+    if(newToken != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(newToken);
+        newToken = INVALID_HANDLE_VALUE;
+    }
+    if(pi.hThread != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(pi.hThread);
+        pi.hThread = INVALID_HANDLE_VALUE;
+    }
+    if(pi.hProcess != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(pi.hProcess);
+        pi.hProcess = INVALID_HANDLE_VALUE;
+    }
+
+    if(cmdline)
+    {
+#ifdef LOOP_WORKER
+        goto loop;
+#else
+        free(cmdline);
+#endif
+    }
+    return exitcode;
 }
 
-void ServiceMain(unsigned long argc, TCHAR *argv[])
+void WINAPI ServiceMain(DWORD argc, TCHAR *argv[])
 {
+    DWORD exitcode = ERROR_SUCCESS;
     TCHAR *cmdline = TEXT("cmd.exe");
-    HANDLE workerHandle;
+    HANDLE workerHandle = INVALID_HANDLE_VALUE;
 
     logf("ServiceMain: start");
+
     g_Status.dwServiceType        = SERVICE_WIN32;
     g_Status.dwCurrentState       = SERVICE_START_PENDING;
     // SERVICE_ACCEPT_SESSIONCHANGE allows us to receive session change notifications.
@@ -153,35 +316,75 @@ void ServiceMain(unsigned long argc, TCHAR *argv[])
     g_Status.dwCheckPoint         = 0;
     g_Status.dwWaitHint           = 0;
     g_StatusHandle = RegisterServiceCtrlHandlerEx(SERVICE_NAME, ControlHandlerEx, NULL);
-    if (g_StatusHandle == 0)
+    if(!g_StatusHandle)
     {
-        perror("RegisterServiceCtrlHandlerEx");
-        goto stop;
+        exitcode = GetLastError();
+        logf_gle("Error: RegisterServiceCtrlHandlerEx failed");
+        // g_StatusHandle isn't set to INVALID_HANDLE_VALUE because it's a not a normal handle
+        goto cleanup;
+    }
+
+    // Create trigger event for the worker thread.
+    g_ConsoleEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+    if(!g_ConsoleEvent)
+    {
+        exitcode = GetLastError();
+        logf_gle("Error: CreateEvent failed");
+        g_ConsoleEvent = INVALID_HANDLE_VALUE;
+        goto cleanup;
     }
 
     g_Status.dwCurrentState = SERVICE_RUNNING;
     SetServiceStatus(g_StatusHandle, &g_Status);
 
-    // Create trigger event for the worker thread.
-    g_ConsoleEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
-
     // Start the worker thread.
     logf("Starting worker thread");
     workerHandle = CreateThread(NULL, 0, WorkerThread, cmdline, 0, NULL);
-    if (!workerHandle)
+    if(!workerHandle)
     {
-        perror("CreateThread");
-        goto stop;
+        exitcode = GetLastError();
+        logf_gle("Error: CreateThread failed");
+        workerHandle = INVALID_HANDLE_VALUE;
+        goto cleanup;
     }
 
     // Wait for the worker thread to exit.
-    WaitForSingleObject(workerHandle, INFINITE);
+    if(WaitForSingleObject(workerHandle, INFINITE) == WAIT_FAILED)
+    {
+        exitcode = GetLastError();
+        logf_gle("Error: WaitForSingleObject failed");
+        goto cleanup;
+    }
 
-stop:
+    if(!GetExitCodeThread(workerHandle, &exitcode))
+    {
+        exitcode = GetLastError();
+        logf_gle("Error: GetExitCodeThread failed");
+        goto cleanup;
+    }
+
+cleanup:
     logf("exiting");
-    g_Status.dwCurrentState = SERVICE_STOPPED;
-    g_Status.dwWin32ExitCode = GetLastError();
-    SetServiceStatus(g_StatusHandle, &g_Status);
+
+    if(g_StatusHandle)
+    {
+        g_Status.dwCurrentState = SERVICE_STOPPED;
+        g_Status.dwWin32ExitCode = exitcode;
+        SetServiceStatus(g_StatusHandle, &g_Status);
+        // The service status handle is not a normal handle (it's a struct) and isn't closed.
+    }
+
+    if(g_ConsoleEvent != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(g_ConsoleEvent);
+        g_ConsoleEvent = INVALID_HANDLE_VALUE;
+    }
+
+    if(workerHandle != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(workerHandle);
+        workerHandle = INVALID_HANDLE_VALUE;
+    }
 
     logf("ServiceMain: end");
     return;
@@ -190,9 +393,9 @@ stop:
 void SessionChange(DWORD eventType, WTSSESSION_NOTIFICATION *sn)
 {
     if (eventType < RTL_NUMBER_OF(g_SessionEventName))
-        logf("SessionChange: %s, session ID %d", g_SessionEventName[eventType], sn->dwSessionId);
+        logf("SessionChange: %s, session ID %I32u", g_SessionEventName[eventType], sn->dwSessionId);
     else
-        logf("SessionChange: <unknown event: %d>, session id %d", eventType, sn->dwSessionId);
+        logf("SessionChange: <unknown event: %I32u>, session id %I32u", eventType, sn->dwSessionId);
 
     if (eventType == WTS_CONSOLE_CONNECT)
     {
@@ -202,7 +405,7 @@ void SessionChange(DWORD eventType, WTSSESSION_NOTIFICATION *sn)
     }
 }
 
-DWORD ControlHandlerEx(DWORD control, DWORD eventType, void *eventData, void *context)
+DWORD WINAPI ControlHandlerEx(DWORD control, DWORD eventType, void *eventData, void *context)
 {
     switch(control)
     {
@@ -219,7 +422,7 @@ DWORD ControlHandlerEx(DWORD control, DWORD eventType, void *eventData, void *co
             break;
 
         default:
-            logf("ControlHandlerEx: code 0x%x, event 0x%x", control, eventType);
+            logf("ControlHandlerEx: code 0x%I32x, event 0x%I32x", control, eventType);
             break;
     }
 
--- README.md
+++ README.md
@@ -4,3 +4,46 @@ QTestService
 QTestService - Service for running a process on the Winlogon desktop
 
 This is my fork of omeg's QTestService from his article [Running processes on the Winlogon desktop](http://omeg.pl/blog/2014/01/running-processes-on-the-winlogon-desktop/).
+
+Please heed to the author's warning: ***[Running a process on the Winlogon desktop] is a Bad Idea unless you know exactly what you’re doing, why you’re doing it and there are no alternatives.***
+
+
+Know This
+---------
+
+### There is a delay before your process is created.
+
+On the Windows 7 x64 SP1 computer that I am running QTestService on it can take up to 10 seconds before the service receives notification that the console has been connected to a different session. I don't know what causes the delay. QTestService only starts your process after it has received the notification.
+
+### Error: CreateProcessAsUser failed, GetLastError: 5
+
+If your process is being created in a session that has already ended you will see this message in the log. It is a likely message if you are quickly switching between two already logged in users because in that case the switch user session only exists during the switch. As noted above there can be a slight delay between what actually is happening and the notification messages received, so unless you see this error all the time assume it failed because the session ended before QTestService received notification.
+
+### There are multiple Winlogon desktops.
+
+Although we may refer to a singular Winlogon desktop there is actually a Winlogon desktop for each session. Therefore if multiple sessions exist there are multiple Winlogon desktops. This service only executes your process in the session that you next connect to after the service is started (unless you have defined `LOOP_WORKER` -- see below).
+
+### A process can be executed multiple times for a session.
+
+If QTestService is built with `LOOP_WORKER` defined then for each console connection a process is executed.
+
+For example:
+- You start QTestService
+- You, a logged in user in session 1, switch to a different logged in user in session 4.
+- Your process is executed on the Winlogon desktop in session 4.
+- You, in session 4, switch back to the user in session 1.
+- Your process is executed on the Winlogon desktop in session 1.
+- You, in session 1, switch back to the user in session 4.
+- Your process is executed on the Winlogon desktop in session 4.
+
+### The Winlogon desktop has a ****very**** small desktop heap.
+
+All user objects (read: GUI stuff) consume memory from the heap of the desktop they are on. Be careful you don't run a process that could exhaust a Winlogon desktop heap. Its heap is 20-150 times smaller than an interactive desktop heap.
+
+### Reference
+
+[Sessions, Desktops and Windows Stations](http://blogs.technet.com/b/askperf/archive/2007/07/24/sessions-desktops-and-windows-stations.aspx)
+[Desktop Heap Overview](http://blogs.msdn.com/b/ntdebugging/archive/2007/01/04/desktop-heap-overview.aspx)
+[Desktop Heap Monitor for Windows Vista, Server 2008 and 7](http://blog.airesoft.co.uk/2009/10/desktop-heap-monitor-vista-7/)
+[Pushing the Limits of Windows: USER and GDI Objects](http://blogs.technet.com/b/markrussinovich/archive/2010/02/24/3315174.aspx)
+[Running processes on the Winlogon desktop](http://omeg.pl/blog/2014/01/running-processes-on-the-winlogon-desktop/)
